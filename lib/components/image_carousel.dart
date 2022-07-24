202d5f404fd6c1db42cd264486f759c65fdd43b3
David Mahbubi <ulrichdavid0370@gmail.com>
2022-07-24 14:38:36 +0700

    Updated homepages banner

--- lib/components/image_carousel.dart
+++ lib/components/image_carousel.dart
@@ -2,17 +2,25 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class ImageCarousel extends StatelessWidget {
+
+  final List<String> carouselImages = const [
+    'geometri_ruang.jpg',
+    'hak_kewajiban.jpg',
+    'indahnya_saling_menghormati.jpg',
+    'kerukunan_dalam_perbedaan.jpg',
+  ];
+
   const ImageCarousel({super.key});
 
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
       options: CarouselOptions(height: 200, enlargeCenterPage: true),
-      items: [1, 2, 3, 4, 5].map((i) {
+      items: carouselImages.map((String carouselFileName) {
         return Builder(builder: (BuildContext context) {
             return ClipRRect(
               borderRadius: const BorderRadius.all(Radius.circular(20)),
-              child: Image.asset('assets/images/example_banner.jpg'),
+              child: Image.asset('assets/images/carousels/$carouselFileName'),
             );
           },
         );
